3ec55958166aedc20a9dafe1f3c30604d79ab687
cudenvn <cudenvn@gmail.com>
2020-12-04 18:39:34 +0000

    day 3a

--- day3a.rb
+++ day3a.rb
@@ -1,1 +1,13 @@
-input = open("day3.txt").read.lines
+input = open("day3.txt").read.lines.map(&:strip)
+
+pos = 0
+trees = 0
+
+input.shift
+
+for i in input
+  pos += 3
+  trees += 1 if i[pos % i.length].include?("#")
+end
+
+puts trees
