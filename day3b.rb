9a82c403ca5d79ca4acd9855ee7ff7edca984a06
cudenvn <cudenvn@gmail.com>
2020-12-05 00:36:28 +0000

    day 3b

--- day3b.rb
+++ day3b.rb
@@ -1,1 +1,22 @@
-input = open("day3.txt").read.lines.map(&:split)
+input = open("day3.txt").read.lines.map(&:strip)
+
+results = 1
+arr = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+input.shift
+
+arr.each do |a|
+  pos = 0
+  trees = 0
+  skip = a[1]
+  for i in 1..input.length
+    next if i % skip != 0
+    pos += a[0]
+    trees += 1 if input[i - 1][pos % input[i - 1].length].include?("#")
+  end
+
+  results *= trees
+end
+
+puts results
+
+# THIS IS DISGUSTING, I HAVE NOT GOT THe FAINTEST CLUE WHY IT WORKS, NEXT!
---
+++ day4a.rb
@@ -0,0 +1,1 @@
+input = open("day4.txt").read.lines
