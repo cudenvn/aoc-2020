a01cd93592da03b1b6d19aa5597b69d0f0906822
cudenvn <cudenvn@gmail.com>
2020-12-06 01:40:43 +0000

    4a

--- day4a.rb
+++ day4a.rb
@@ -1,1 +1,9 @@
-input = open("day4.txt").read.lines
+input = open("day4.txt").read.split("\n\n").map { |s| s.split(/\s+/) }
+
+count = 0
+
+input.each do |passport|
+  count += 1 if passport.count == 8 || passport.count == 7 && !passport.any? { |key| key.start_with?("cid") }
+end
+
+puts count
