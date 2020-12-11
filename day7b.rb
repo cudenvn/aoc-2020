709135a2eb251c6819751dbb75ca64e97d3a4ed4
cudenvn <cudenvn@gmail.com>
2020-12-11 13:03:35 +0000

    forgive me I'm dumb

--- day7b.rb
+++ day7b.rb
@@ -1,14 +1,28 @@
-input = open("day7.txt").read.split("\n").map { |s| s.gsub(/(bag(s*))/, "").split(/(?:contain)|[,.]/).map(&:strip) }
+$input = open("day7.txt").read.split("\n").map { |s| s.gsub(/(bag(s*))/, "").split(/(?:contain)|[,.]/).map(&:strip) }
 
-searching = ["shiny gold"]
+def method_name(arr)
+  return 0 if arr.uniq.size == 1 && arr.uniq.first == "no other"
 
-for i in searching
-  for j in input
-    if j.any? { |s| s.include?(i) }
-      searching << j[0] unless (j[0] == i || searching.any?(j[0]))
+  count = 0
+  spread = []
+  arr.each do |a|
+    next if a == "no other"
+    bags = $input.find { |i| i.first == a }.slice(1..)
+    bags.each do |b|
+      if b == "no other"
+        spread << b
+        next
+      end
+
+      c = b.split(" ", 2)
+      count += c[0].to_i
+      c[0].to_i.times do
+        spread << c[1]
+      end
     end
   end
+
+  return count + method_name(spread)
 end
 
-searching.shift
-p searching.count
+puts method_name(["shiny gold"])
